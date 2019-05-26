f885b26b9696faa90bd24422001874a335c2c17b
Salem <salimmagdi@gmail.com>
2019-05-26 02:38:40 +0200

    student attributes form

--- src/app/students/student-admission/student-admission.component.ts
+++ src/app/students/student-admission/student-admission.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { NgForm } from '@angular/forms';
 
 @Component({
     selector: 'app-student-admission',
@@ -7,13 +8,47 @@ import { Component, OnInit } from '@angular/core';
 })
 export class StudentAdmissionComponent implements OnInit {
 
-    currentStep = 4;
+    currentStep = 1;
     constructor() { }
-
+    countries = ['Egypt'];
+    @ViewChild('applicationAttributesForm') applicationAttributesForm: NgForm;
     ngOnInit() {
 
     }
     changeStep(step) {
         this.currentStep = step;
+    }
+    // {
+    //     "application": {
+    //         "status": "test",
+    //         "application_fees": 14,
+    //         "tuition_fees": 33,
+    //         "applicant_attributes": {
+    //             "first_name": "amr",
+    //             "last_name": "adel",
+    //             "birth_date": "2015-12-08",
+    //             "mobile_number": "23234",
+    //             "landline": "",
+    //             "email": "student@gmail.com",
+    //             "birth_country": "Egypt",
+    //             "birth_city": "Giza",
+    //             "primary_address": "Imbaba",
+    //             "secondary_address": "lewa",
+    //             "guardians_attributes": [
+    //                 {
+    //                     "name": "khalil",
+    //                     "relation": "Parent",
+    //                     "primary_address": "Imbaba",
+    //                     "secondary_address": "lewa",
+    //                     "mobile_number": "01112442424",
+    //                     "email": "parent@gmail.com"
+    //                 }	
+    //             ]
+    //         }
+    //     }
+    // }
+
+    OnApplicationAttributesSubmit() {
+        console.log(this.applicationAttributesForm.value)
     }
 }
